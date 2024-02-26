0e7950b8228c7da593aeffdf58c8c00b5354b22c
Vaigarai14 <vaigaraithennarasu144@gmail.com>
2024-02-26 17:54:30 +0530

    learned local storage and how to use it in react

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -12,12 +12,33 @@ marked completed [btn]
 */
 
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const Todolist = () => {
 
+
+
     let [inputvalue, setinputvalue] = useState("")
     const [task, settask] = useState([])
+
+    useEffect(() => {
+
+        const stritem = JSON.stringify([...task])
+
+        task.length > 0 && window.localStorage.setItem('listitem', [stritem])
+
+        return () => {
+            window.localStorage.removeItem("")
+        }
+    }, [task])
+
+    useEffect(() => {
+        let storageitem = window.localStorage.getItem("listitem")
+        let obj = JSON.parse(storageitem)
+        if (storageitem) {
+            settask(obj)
+        }
+    }, [])
 
     const saveinput = (e) => {
         setinputvalue(e.target.value)
@@ -37,9 +58,22 @@ export const Todolist = () => {
         }))
     }
 
+
+
+    const deleteitem = (id) => {
+        const updateditem = task.filter(item => item.id !== id)
+        settask(updateditem)
+        window.localStorage.removeItem('listitem')
+        console.log(window.localStorage.clear
+            
+            );
+    };
+
+
+
     return (
         <div className="my-12 ">
-            <div className='border-2 w-fit p-4 px-24 m-auto rounded-xl'>
+            <div className='border-4 w-fit p-4 px-24 m-auto rounded-xl'>
                 <h1 className='text-6xl text-center font-bold m-4 p-2'>To do List</h1>
                 <div className="flex justify-center">
                     <input
@@ -50,11 +84,16 @@ export const Todolist = () => {
                         onClick={added}
                         className=' m-2 text-lg font-semibold bg-green-200 rounded-lg px-12 py-1'>Add</button>
                 </div>
-                <div className="text-lg">
+                <div className="m-4 text-lg">
                     {task.map(item => (
                         <li key={item.id} style={{ listStyle: 'none', display: "flex", alignItems: "center", justifyContent: 'center' }} className={item.completed ? 'completed' : ''}>
                             <input className='size-5 items-center ' type="checkbox" checked={item.completed} onChange={() => { togglebtn(item.id) }} />
-                            <span className="w-36 my-2 mx-4 text-left">{item.text}</span>
+                            <span className="w-48 my-3 mx-4 text-left font-semibold text-xl capitalize">{item.text}</span>
+                            <button
+                                onClick={() => {
+                                    deleteitem(item.id)
+                                }}
+                                className='px-4 py-1 bg-gray-800 text-white rounded-lg flex items-center'>Delete</button>
                         </li>
                     )
                     )}
